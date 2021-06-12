bac1ad7ab9927d08ba57bc4526aedcb3facc7ee0
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-06-12 01:37:42 -0600

    Fixed responsive design

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-//what do i need to make this menu?
 
-//onclick of the burger
-
-const burgerButton = document.querySelector('.burger');
-const list = document.querySelector('.navigation-links');
 const listItems = document.querySelectorAll('.navigation-links ul li');
-const productChildren = document.querySelector('.Product-children');
-const companyChildren = document.querySelector('.Company-children');
-const connectChildren = document.querySelector('.Connect-children');
-const itemOne = document.querySelector('.navigation-links, .item-1');
-const itemTwo = document.querySelector('.navigation-links, .item-2');
-const itemThree = document.querySelector('.navigation-links, .item-3');
 let itemNumber = 0;
-//give each list item a class .list-items 
+////give each list item a class .list-items 
 listItems.forEach(item => {
     itemNumber++
     item.classList.add('list-item');
     item.classList.add(`item-${itemNumber}`);
 });
 
+const burgerButton = document.querySelector('.burger');
+const list = document.querySelector('.navigation-links');
+const productChildren = document.querySelector('.Product-children');
+const companyChildren = document.querySelector('.Company-children');
+const connectChildren = document.querySelector('.Connect-children');
+const itemOne = document.querySelector('.item-1 ');
+const itemTwo = document.querySelector('.item-2');
+const itemThree = document.querySelector('.item-3');
+
+
+//mobile view javascript
 
 //event listeners for burger and inside burger menu
 burgerButton.addEventListener('click', () => {
@@ -48,6 +48,7 @@ itemThree.addEventListener('click', () => {
 });
 
 
+// desktop view javascript
 
 
 
